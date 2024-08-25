bab2bbaaa52c15d5311bbf185a5f2e41acdc2325
Suraj Kumar <sk5195609@gmail.com>
2024-08-25 14:27:20 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+
 
 app = Flask(__name__)
-CORS(app)  # This will allow all domains; you can customize this as needed
+ # This will allow all domains; you can customize this as needed
 
 # Your existing code...
 
